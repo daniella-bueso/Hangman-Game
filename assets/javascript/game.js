68427e7036c31af5e819dd1997fca76993df00e8
unknown <danielladevoto@gmail.com>
2017-11-17 14:24:50 -0500

    progress with js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,70 @@
+// Only works with jquery
+$(document).ready(function() {
+
 // VARIABLES
-	// Arrays for categories in game
-	var countryName=["United States","Canada", "France", "Spain", "Germany", "Peru", "Brazil", "Argentina", "Japan", "Australia", "China"];
+	// Arrays with words for game
+	var countryName=["UNITED STATES","CANADA", "FRANCE", "SPAIN", "GERMANY", "PERU", "RUSSIA", "ARGENTINA", "JAPAN", "AUSTRALIA", "CHINA"];
+	// Chooses random word from category
+	var randomWord = countryName[Math.floor(Math.random() * countryName.length)];
+
 	// var sportName=["Soccer", "Football", "Baseball", "Voleyball", "Swimming", "Tennis", "Track", "Wrestling", "Basketball", "Lacrosse", "Water Polo", "Field Hockey" ];
 	// var showName=["Game of Thrones", "Big Bang Theory", "Stranger Things", "House of Cards", "Friends", "The Flash", "Black Mirror", "How I Met Your Mother", "The Arrow"];
 	// var animalName=["dog", "cat", "deer", "wolf", "giraffe", "polar bear", "alligator", "elephant", "panther", "shark", "dolphin"];
 
-// variables to number of wins, letters used, and guesses left 
+// Variables to keep count of wins, letters used, guesses left and array for answers
     var wins = 0;
     var lettersGuessed = 0;
-    var guessesLeft = 0; 
+    var guessesLeft = 10;
+    var answerArray= [];
+    var w;
 
-// FUNCTIONS
-
-
-
-
-
-// MAIN PROCESS
-// any pressed key will start game  
+// Any key pressed starts game
 	document.onkeyup = function(event) {
-		var startGame = event.key;
+		startGame ();
+// FUNCTIONS
+	function startGame () {
+
+		for (var i = 0; i < randomWord.length; i++) {
+			answerArray[i]= "_";
+		}
+
+		w = answerArray.join = (" ");
+		document.getElementById("random-word").innerHTML = "Word is " + w;
+
 	}
 
-	function (startGame) {
-		// Chooses random word from category
-		var chosenWord = countryName[Math.floor(Math.random() * countryName.length)];
-		// Splits the chosen word into individual letters
-		lettersInWord = chosenWord.split ('');
-		num.Blanks = lettersInWord.length;
-		
+	function letter () {
+		// letter typed by user goes into assigned box
+		var letter = document.getElementById("letters").innerHTML = ("Letters already guessed: " + value);
+
+		// Checks if the user has typed a letter
+		if (letter.length > 0) {
+			for (var i = 0; i < randomWord.length; i++) {
+				
+				// Checks if random word contains the latter the user pressed
+				if (randomWord[i] === letter) {
+					answerArray[i] = letter;
+				}
+			}
+			//count the user's guesses
+			guessesLeft--;
+			document.getElementById("guesses").innerHTML = "Guesses left: <br>" + guessesLeft;
+		} 
+
+		if (guessesLeft < 0) {
+			document.getElementById("random-word").innerHTML = "You lose!";
+		}
+
 
 	}
+}
 
+})
 	
 
 // build results into a variable to output to HTML;
-    var printWins = document.getElementById('wins').innerHTML = wins;
-    var printLetters = document.getElementById('letters').innerHTML = letters;
-    var printGuesses = document.getElementById('guesses').innerHTML = guesses;
+   //  var printWins = document.getElementById("wins").innerHTML = 1;
+   //  var printLetters = document.getElementById('letters').innerHTML = letters;
+   //  var printGuesses = document.getElementById('guesses').innerHTML = guesses;
    
-    }
+   // })
